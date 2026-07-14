354e04bcd84cae9797fdb2eb9c075cfaecaeb47c
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2026-07-14 17:21:46 +0100

    CRUD Modifications

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -3,7 +3,5 @@ using Microsoft.AspNetCore.Identity;
 namespace AeroResponse.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser
-{
-}
+public class ApplicationUser : IdentityUser;
 
